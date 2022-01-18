3530b601276ccf24e7deb86ab91c58e8b9ef646b
“Leonardo” <89543577+leotanaka4@users.noreply.github.com>
2022-01-18 10:01:24 -0300

    Montadoras

--- silver-motors/src/App.js
+++ silver-motors/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <body id="app">
+    <div id="app">
       <div id="cabecalho">
         <div id="titulo">
           <h1>SILVER MOTORS</h1>
@@ -13,12 +13,61 @@ function App() {
         </div>
       </div>
       <div id="conteudo">
-
+        <div id="historia">
+          <h2>Quem nós somos?</h2>
+          <p>
+            Somos uma empresa de vendas de carros novos e seminovos
+            há mais de 3 anos no mercado com ótimos profissionais e técnicos
+            que irão te auxiliar a conseguir o sonho de adquirir um carro.
+          </p>
+        </div>
+        <div id="montadoras">
+          <div class="carros">
+            <h3>CHERY:</h3>
+          </div>
+          <div class="carros">
+            <h3>CHEVROLET:</h3>
+          </div>
+          <div class="carros">
+            <h3>CITROEN::</h3>
+          </div>
+          <div class="carros">
+            <h3>FIAT:</h3>
+          </div>
+          <div class="carros">
+            <h3>FORD:</h3>
+          </div>
+          <div class="carros">
+            <h3>HONDA:</h3>
+          </div>
+          <div class="carros">
+            <h3>HYUNDAI:</h3>
+          </div>
+          <div class="carros">
+            <h3>JEEP:</h3>
+          </div>
+          <div class="carros">
+            <h3>NISSAN:</h3>
+          </div>
+          <div class="carros">
+            <h3>PEUGEOT:</h3>
+          </div>
+          <div class="carros">
+            <h3>RENAULT:</h3>
+          </div>
+          <div class="carros">
+            <h3>TOYOTA:</h3>
+          </div>
+          <div class="carros">
+            <h3>VOLKSWAGEN:</h3>
+          </div>
+        </div>
       </div>
       <div id = "rodape">
-
+        <h3>Telefone de contato:</h3>
+        <p>+55 21 2498-2088</p>
       </div>
-    </body>
+    </div>
   );
 }
 
