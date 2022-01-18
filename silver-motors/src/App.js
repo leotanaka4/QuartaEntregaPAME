5020b40a71171c3f836e9d75018b9daaedb940ff
“Leonardo” <89543577+leotanaka4@users.noreply.github.com>
2022-01-17 21:34:23 -0300

    Salvar

---
+++ silver-motors/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      <div id="cabecalho">
+        <div id="titulo">
+          <h1>Silver Motors</h1>
+        </div>
+        <div id="pesquisa">
+
+        </div>
+      </div>
+      <div id="lista">
+
+      </div>
+      <div id="informacoes">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
